be9bd3d24a5f43c236f622a171e07b66fabcaf03
estellegomez31-Igtm <estelle.gomez31@gmail.com>
2026-08-14 15:19:12 +0200

    Utiliser VITE_API_URL en production

--- step-7/frontend/src/views/Login.vue
+++ step-7/frontend/src/views/Login.vue
@@ -21,7 +21,7 @@ async function connect() {
 </script>
 
 <template>
- 
+ <div class="hero"></div>
     <div class="logo">
   <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="34" height="34">
     <path d="M3 11L12 3L21 11" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
